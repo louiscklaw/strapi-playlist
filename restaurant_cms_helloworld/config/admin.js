0f7348cd98a51c782d5e3768b3d6c3b59e210974
Louis Law <louiscklaw@users.noreply.github.com>
2022-04-01 02:17:49 +0800

    update restaurant_cms_helloworld,

--- restaurant_cms_helloworld/config/admin.js
+++ restaurant_cms_helloworld/config/admin.js
@@ -2,5 +2,5 @@ module.exports = ({ env }) => ({
   auth: {
     secret: env("ADMIN_JWT_SECRET", "312e8318f78481ceed32ee331290ef06"),
   },
-  watchIgnoreFiles: ["**/config/sync/**"],
+  watchIgnoreFiles: ["**/config/sync/**", "**/_backup/**"],
 });
