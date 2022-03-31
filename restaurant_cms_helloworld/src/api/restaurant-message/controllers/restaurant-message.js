d8bb548fb65baab00e2ad9df211df37737160c18
Louis Law <louiscklaw@users.noreply.github.com>
2022-04-01 02:36:40 +0800

    update restaurant_cms_helloworld,

---
+++ restaurant_cms_helloworld/src/api/restaurant-message/controllers/restaurant-message.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ *  restaurant-message controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::restaurant-message.restaurant-message');
