4218c8667b7500070543b2ebfa7238d991dafbf6
louiscklaw <logickee@gmail.com>
2022-04-03 03:20:51 +0800

    update requests_responses_tryout/_helloworld_tryout,

--- plugins/helloworld/src/api/restaurant/controllers/restaurant.js
+++ plugins/helloworld/src/api/restaurant/controllers/restaurant.js
@@ -11,9 +11,6 @@ module.exports = createCoreController(
   ({ strapi }) => ({
     // Method 2: Wrapping a core action (leaves core logic in place)
     async helloRestaurants(ctx) {
-      // some custom logic here
-      ctx.query = { ...ctx.query, local: "en" };
-
       // Calling the default core action
       const { data, meta } = await super.find(ctx);
 
