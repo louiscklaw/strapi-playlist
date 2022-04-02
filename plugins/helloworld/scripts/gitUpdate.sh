072e6b5c5556a415a49e2df6ecacba6e8b0900ad
louiscklaw <logickee@gmail.com>
2022-04-03 02:55:23 +0800

    update requests_responses_tryout/_helloworld_tryout,

---
+++ plugins/helloworld/scripts/gitUpdate.sh
@@ -0,0 +1,6 @@
+git add .
+git commit -m"update requests_responses_tryout/_helloworld_tryout,"
+
+git pull
+
+git push
---
+++ plugins/helloworld/src/api/restaurant/controllers/restaurant.js
@@ -0,0 +1,26 @@
+"use strict";
+
+/**
+ *  restaurant controller
+ */
+
+const { createCoreController } = require("@strapi/strapi").factories;
+
+module.exports = createCoreController(
+  "api::restaurant.restaurant",
+  ({ strapi }) => ({
+    // Method 2: Wrapping a core action (leaves core logic in place)
+    async helloRestaurants(ctx) {
+      // some custom logic here
+      ctx.query = { ...ctx.query, local: "en" };
+
+      // Calling the default core action
+      const { data, meta } = await super.find(ctx);
+
+      // some more custom logic
+      meta.date = Date.now();
+
+      return { data, meta };
+    },
+  })
+);
