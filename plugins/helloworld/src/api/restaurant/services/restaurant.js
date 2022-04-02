78694eafc3474cb53301d177183a6faa4424f18f
louiscklaw <logickee@gmail.com>
2022-04-03 03:47:40 +0800

    update requests_responses_tryout/_helloworld_tryout,

--- plugins/helloworld/src/api/restaurant/services/restaurant.js
+++ plugins/helloworld/src/api/restaurant/services/restaurant.js
@@ -1,9 +1,25 @@
-'use strict';
+"use strict";
 
 /**
  * restaurant service.
  */
 
-const { createCoreService } = require('@strapi/strapi').factories;
+const { createCoreService } = require("@strapi/strapi").factories;
 
-module.exports = createCoreService('api::restaurant.restaurant');
+module.exports = createCoreService(
+  "api::restaurant.restaurant",
+  ({ strapi }) => ({
+    // Method 1: Creating an entirely custom service
+    async find(...args) {
+      // Calling the default core controller
+      const { results, pagination } = await super.find(...args);
+
+      // some custom logic
+      results.forEach((result) => {
+        result.counter = 1;
+      });
+
+      return { results, pagination };
+    },
+  })
+);
