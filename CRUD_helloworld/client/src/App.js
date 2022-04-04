64ede3531b2825221a19ea68dc4d3c0c8c01257a
Louis Law <louiscklaw@users.noreply.github.com>
2022-04-05 02:35:59 +0800

    update CRUD_helloworld,

--- CRUD_helloworld/client/src/App.js
+++ CRUD_helloworld/client/src/App.js
@@ -6,7 +6,19 @@ import React from "react";
 
 function App() {
   const [jwt_token, setJwtToken] = React.useState(null);
+
   const [test_data, setTestData] = React.useState(null);
+
+  const [id_to_read, setIdToRead] = React.useState(null);
+  const [read_response, setReadResponse] = React.useState();
+
+  const [id_to_put, setIdToput] = React.useState(null);
+  const [title_to_put, setTitleToPut] = React.useState();
+  const [put_response, setPutResponse] = React.useState();
+
+  const [id_to_delete, setIdToDelete] = React.useState(null);
+  const [delete_response, setDeleteResponse] = React.useState();
+
   const [username_login, setUsernameLogin] = React.useState("test1@test.com");
   const [password_login, setPasswordLogin] = React.useState("test1@test.com");
   const checkLogin = () => {
@@ -92,12 +104,44 @@ function App() {
   };
   const onReadClick = () => {
     console.log("onReadClick");
+    axios
+      .get(`http://localhost:1337/api/articles/${id_to_read}`, {
+        Authorization: `Bearer ${jwt_token}`,
+      })
+      .then((response) => {
+        setReadResponse(JSON.stringify(response.data, null, 2));
+      })
+      .catch((error) => {
+        console.log("An error occurred:", error.response);
+      });
   };
   const onUpdateClick = () => {
     console.log("onUpdateClick");
+    axios
+      .put(
+        `http://localhost:1337/api/articles/${id_to_put}`,
+        { data: { title: title_to_put } },
+        { Authorization: `Bearer ${jwt_token}` }
+      )
+      .then((response) => {
+        setPutResponse(JSON.stringify(response));
+      })
+      .catch((error) => {
+        console.log("An error occurred:", error.response);
+      });
   };
   const onDeleteClick = () => {
     console.log("onDeleteClick");
+    axios
+      .delete(`http://localhost:1337/api/articles/${id_to_delete}`, {
+        Authorization: `Bearer ${jwt_token}`,
+      })
+      .then((response) => {
+        setDeleteResponse(JSON.stringify(response));
+      })
+      .catch((error) => {
+        console.log("An error occurred:", error.response);
+      });
   };
 
   return (
@@ -169,19 +213,29 @@ function App() {
       <div style={{ padding: "1rem" }}>
         <div>CRUD helloworld</div>
         <div>
-          <input type="text" onChange={(e) => setTestData(e.target.value)} />
+          <div>
+            <input type="text" onChange={(e) => setTestData(e.target.value)} />
+
+            <button onClick={(e) => onCreateClick(e)}>create</button>
+          </div>
         </div>
         <div>
-          <button onClick={(e) => onCreateClick(e)}>create</button>
-        </div>
-        <div>
+          <input type="text" onChange={(e) => setIdToRead(e.target.value)} />
           <button onClick={(e) => onReadClick(e)}>read</button>
+          <pre>{read_response}</pre>
         </div>
         <div>
+          <input type="text" onChange={(e) => setIdToput(e.target.value)} />
+          <input type="text" onChange={(e) => setTitleToPut(e.target.value)} />
+
           <button onClick={(e) => onUpdateClick(e)}>update</button>
+          <pre>{put_response}</pre>
         </div>
         <div>
+          <input type="text" onChange={(e) => setIdToDelete(e.target.value)} />
+
           <button onClick={(e) => onDeleteClick(e)}>delete</button>
+          <pre>{delete_response}</pre>
         </div>
       </div>
     </div>
