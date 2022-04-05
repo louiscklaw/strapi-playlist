47c40e303b38802cd4d9a40c0fa6956f51f0263f
Louis Law <louiscklaw@users.noreply.github.com>
2022-04-05 19:42:47 +0800

    update foodadvisor,

---
+++ content_draft_helloworld/api/src/api/restaurant-review/controllers/restaurant-review.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ *  restaurant-review controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::restaurant-review.restaurant-review');
---
+++ content_draft_helloworld/api/src/api/restaurant-review/routes/restaurant-review.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * restaurant-review router.
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::restaurant-review.restaurant-review');
