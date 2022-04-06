6d0c452c03fa306f9075df2f35f8bc31e2938c5c
Louis Law <louiscklaw@users.noreply.github.com>
2022-04-06 18:41:17 +0800

    update,

---
+++ swagger_helloworld/scripts/dev.ps1
@@ -0,0 +1,7 @@
+# netstat -ano | findstr :<PORT>
+# taskkill /PID <PID> /F
+npx kill-port 1337
+
+yarn --dev
+
+yarn develop
---
+++ swagger_helloworld/scripts/install.ps1
@@ -0,0 +1,6 @@
+# seed.js
+# yarn add -D unzip-stream uuid-v4
+
+yarn --dev
+
+yarn add --ignore-engines @strapi/plugin-documentation
