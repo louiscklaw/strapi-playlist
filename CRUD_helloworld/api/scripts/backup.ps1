3d97a22b2c25a6a1e01dd2e018c01c64d9636fcd
Louis Law <louiscklaw@users.noreply.github.com>
2022-04-08 18:14:12 +0800

    update CRUD_helloworld,

---
+++ CRUD_helloworld/api/scripts/backup.ps1
@@ -0,0 +1,13 @@
+# sqlite3 D:\_workspace\strapi-playlist\restaurant_cms_helloworld\.tmp\data.db
+
+# sqlite> .output c:/Temp/test.sql
+# sqlite> .dump
+# sqlite> .exit
+
+sqlite3.exe D:\_workspace\strapi-playlist\restaurant_cms_helloworld\.tmp\data.db `
+  ".output c:/Temp/test.sql" `
+  ".dump" `
+  ".exit"
+
+# .read ./backup.sql
+# .exit
